435aa59ba4a882a5f3e262dd9f36d8299411e03d
xuzixuan <yoloxzx@outlook.com>
2025-10-15 18:40:58 +0800

    Merge branch 'dev' of ssh://192.168.1.253:9922/agvc/agvc-admin into dev

--- server/initialize/router_biz.go
+++ server/initialize/router_biz.go
@@ -14,11 +14,6 @@ func initBizRouter(routers ...*gin.RouterGroup) {
 	publicGroup := routers[1]
 	holder(publicGroup, privateGroup)
 	{
-		systemRouter := router.RouterGroupApp.System
-		systemRouter.InitSysGridConnectionPointRouter(privateGroup, publicGroup)
-		systemRouter.InitSysHisEventRouter(privateGroup, publicGroup)
-	}
-	{
 		monitorRouter := router.RouterGroupApp.Monitor
 		monitorRouter.InitInverterMonitorRouter(privateGroup, publicGroup)
 	} // 占位方法，保证文件可以正确加载，避免go空变量检测报错，请勿删除。
