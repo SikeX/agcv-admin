2bceae76677bb76141803cbf1a3de626e7fb2487
xuzixuan <yoloxzx@outlook.com>
2025-12-03 09:47:57 +0800

    页面修改

--- server/model/agvc/request/agvc_power_chart.go
+++ server/model/agvc/request/agvc_power_chart.go
@@ -3,7 +3,7 @@ package request
 // AgvcPowerChartRequest 电站出力图表查询请求
 type AgvcPowerChartRequest struct {
 	Psid      int    `json:"psid" form:"psid" `
-	Eqid      int    `json:"eqid" form:"eqid" binding:"required"`
+	Eqid      int    `json:"eqid" form:"eqid"`
 	StartTime string `json:"startTime" form:"startTime" binding:"required"`
 	EndTime   string `json:"endTime" form:"endTime" binding:"required"`
 }
@@ -11,7 +11,7 @@ type AgvcPowerChartRequest struct {
 // AgvcVoltageReactiveChartRequest 电压和无功出力图表查询请求
 type AgvcVoltageReactiveChartRequest struct {
 	Psid      int    `json:"psid" form:"psid" `
-	Eqid      int    `json:"eqid" form:"eqid" binding:"required"`
+	Eqid      int    `json:"eqid" form:"eqid"`
 	StartTime string `json:"startTime" form:"startTime" binding:"required"`
 	EndTime   string `json:"endTime" form:"endTime" binding:"required"`
 }
